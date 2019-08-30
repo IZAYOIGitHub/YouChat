25d55165764eda38013ff00d975dc94ac04ba722
IZAYOIGitHub <1906826229@qq.com>
2019-08-30 10:34:38 +0800

    init

---
+++ client/client.h
@@ -0,0 +1,9 @@
+/*********************************************************
+  * Filename : client.h
+  * Description : client program's head file
+  * *****************************************************/
+
+typedef struct{
+	char account[32];
+	bool is_onlin:
+}user_info_t;
